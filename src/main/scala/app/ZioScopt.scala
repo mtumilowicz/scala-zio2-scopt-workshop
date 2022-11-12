c65818d591963932afad94e760ddc363011dfbf5
mtumilowicz <michal.tumilowicz01@gmail.com>
2022-11-12 12:15:51 +0100

    removing logging

--- src/main/scala/app/ZioScopt.scala
+++ src/main/scala/app/ZioScopt.scala
@@ -1,28 +1,11 @@
 package app
 
-import org.slf4j.LoggerFactory
-import scopt.{DefaultOEffectSetup, OParser}
+import scopt.OParser
 import zio.{IO, ZIO}
 
 object ZioScopt {
 
-  private val logger = LoggerFactory.getLogger(classOf[Nothing])
-  private val effectSetup = new DefaultOEffectSetup {
-    override def displayToOut(msg: String): Unit = logger.info(msg)
-
-    override def displayToErr(msg: String): Unit = logger.error(msg)
-    // override def reportError(msg: String): Unit = displayToErr("Error: " + msg)
-    // override def reportWarning(msg: String): Unit = displayToErr("Warning: " + msg)
-
-    // ignore terminate
-    override def terminate(exitState: Either[String, Unit]): Unit = ()
-  }
-
   def parse[T](parser: OParser[_, T], options: List[String], init: T): IO[Option[Nothing], T] =
-    ZIO.fromOption(OParser.runParser(parser, options, init) match {
-      case (result, effects) =>
-        OParser.runEffects(effects, effectSetup)
-        result
-    })
+    ZIO.fromOption(OParser.parse(parser, options, init))
 
 }
--- src/test/scala/app/AppSpec.scala
+++ src/test/scala/app/AppSpec.scala
@@ -9,37 +9,49 @@ object AppSpec extends ZIOSpecDefault {
   val subject = App.program
   val sumCommand = Chunk("sum", "--c1", "2", "--c2", "3")
   val multiplicationCommand = Chunk("mult", "--c1", "2", "--c2", "3")
-  def divisionCommand(divisor: Int) = Chunk("div", "--c1", "2", "--c2", divisor.toString)
+  private def divisionCommand(divisor: Int) = Chunk("div", "--c1", "2", "--c2", divisor.toString)
 
-  override def spec= suite("a")(
-    test("empty args => error") {
-      assertZIO(subject.exit)(fails(equalTo(CommandExecutionError.emptyCommand)))
-    }.provideSome[CommandGateway](ZIOAppArgs.empty),
-    test("unknown args => error") {
-      assertZIO(subject.exit)(fails(equalTo(CommandExecutionError.notSupported)))
-    }.provideSome[CommandGateway](argsLayer(Chunk("aaa"))),
-    test("sum") {
-      for {
-        _ <- subject
-        result <- firstOutput
-      } yield assertTrue(result == "5")
-    }.provideSome[CommandGateway](argsLayer(sumCommand)),
-    test("multiplication") {
-      for {
-        _ <- subject
-        result <- firstOutput
-      } yield assertTrue(result == "6")
-    }.provideSome[CommandGateway](argsLayer(multiplicationCommand)),
-    test("division, divisor != 0") {
-      for {
-        _ <- subject
-        result <- firstOutput
-      } yield assertTrue(result == "1")
-    }.provideSome[CommandGateway](argsLayer(divisionCommand(2))),
-    test("division, divisor == 0") {
-      assertZIO(subject.exit)(fails(equalTo(CommandExecutionError.notSupported)))
-    }.provideSome[CommandGateway](argsLayer(divisionCommand(0))),
+  override def spec = suite("a")(
+    emptyArgsError,
+    unknownArgsError,
+    sumSuccess,
+    multiplicationSuccess,
+    divisionNonZeroDivisorSuccess,
+    divisionZeroDivisorError,
   ).provideSome(CommandGateway.live, CommandService.live)
+
+  lazy val emptyArgsError = test("empty args => error") {
+    assertZIO(subject.exit)(fails(equalTo(CommandExecutionError.emptyCommand)))
+  }.provideSome[CommandGateway](ZIOAppArgs.empty)
+
+  lazy val unknownArgsError = test("unknown args => error") {
+    assertZIO(subject.exit)(fails(equalTo(CommandExecutionError.notSupported)))
+  }.provideSome[CommandGateway](argsLayer(Chunk("aaa")))
+
+  lazy val sumSuccess = test("sum => success") {
+    for {
+      _ <- subject
+      result <- firstOutput
+    } yield assertTrue(result == "5")
+  }.provideSome[CommandGateway](argsLayer(sumCommand))
+
+  lazy val multiplicationSuccess = test("multiplication => success") {
+    for {
+      _ <- subject
+      result <- firstOutput
+    } yield assertTrue(result == "6")
+  }.provideSome[CommandGateway](argsLayer(multiplicationCommand))
+
+  lazy val divisionNonZeroDivisorSuccess = test("division, divisor != 0 => success") {
+    for {
+      _ <- subject
+      result <- firstOutput
+    } yield assertTrue(result == "1")
+  }.provideSome[CommandGateway](argsLayer(divisionCommand(2)))
+
+  lazy val divisionZeroDivisorError = test("division, divisor == 0 => error") {
+    assertZIO(subject.exit)(fails(equalTo(CommandExecutionError.notSupported)))
+  }.provideSome[CommandGateway](argsLayer(divisionCommand(0)))
 
   private def argsLayer(chunk: Chunk[String]) = ZLayer.succeed(ZIOAppArgs(chunk))
 
